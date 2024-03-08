b8d232e780a598bac34df3a15abc51fe9f24118f
jongsikk <xxx_@kakao.com>
2024-03-08 23:44:10 +0900

    240308 꼬마정민

---
+++ boj11382.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class boj11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+        BigInteger c = new BigInteger(sc.next());
+
+        System.out.println(a.add(b).add(c));
+    }
+}
