ef120928bc6b4fe7463d528c1dece406209545c9
jongsikk <xxx_@kakao.com>
2024-04-26 21:51:12 +0900

    240426 수들의 합

---
+++ boj1789.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj1789 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        long s = Long.parseLong(reader.readLine());
+        long count = 0;
+        long sum = 0;
+        // if (s == 1) {
+        // System.out.println(1);
+        // } else {
+        while (sum <= s) {
+            count++;
+            sum += count;
+        }
+        System.out.println(count - 1);
+        // }
+    }
+}
