bfcd7da5c458033f07eacb46e0ac5ddacc8b962e
jongsikk <xxx_@kakao.com>
2024-06-09 22:24:12 +0900

    240609 주몽

---
+++ boj1940.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj1940 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int count = 0;
+        int start = 0;
+        int end = n - 1;
+        // 1 2 3 4 5 7
+        while (start < end) {
+            if (arr[start] + arr[end] < m) {
+                start++;
+            } else if (arr[start] + arr[end] > m) {
+                end--;
+            } else {
+                start++;
+                end--;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
