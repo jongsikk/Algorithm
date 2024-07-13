311a15a4d3f79f3dc6846463af8525e78977164b
jongsikk <xxx_@kakao.com>
2024-07-13 20:19:02 +0900

    240713 leetcode27_RemoveElement

---
+++ leetcode27.java
@@ -0,0 +1,13 @@
+public class leetcode27 {
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int count = 0;
+            for (int i : nums) {
+                if (i != val) {
+                    nums[count++] = i;
+                }
+            }
+            return count;
+        }
+    }
+}
