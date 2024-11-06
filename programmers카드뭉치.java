d0e917c7d1cdd1362e24e3fea072c4452ffb2b28
jongsikk <xxx_@kakao.com>
2024-11-06 14:09:58 +0900

    241106 카드 뭉치

---
+++ programmers카드뭉치.java
@@ -0,0 +1,21 @@
+public class programmers카드뭉치 {
+    class Solution {
+        public String solution(String[] cards1, String[] cards2, String[] goal) {
+            int index1 = 0;
+            int index2 = 0;
+            boolean flag = true;
+
+            for (int i = 0; i < goal.length; i++) {
+                if (index1 < cards1.length && goal[i].equals(cards1[index1])) {
+                    index1++;
+                } else if (index2 < cards2.length && goal[i].equals(cards2[index2])) {
+                    index2++;
+                } else {
+                    flag = false;
+                }
+            }
+
+            return flag ? "Yes" : "No";
+        }
+    }
+}
