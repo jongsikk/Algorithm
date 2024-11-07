edd81508e2dea7a84077dc602792525b613ec6fd
jongsikk <xxx_@kakao.com>
2024-11-07 15:06:10 +0900

    241107 추억 점수

---
+++ programmers추억점수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class programmers추억점수 {
+    class Solution {
+        public int[] solution(String[] name, int[] yearning, String[][] photo) {
+            int[] answer = new int[photo.length];
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < name.length; i++) {
+                map.put(name[i], yearning[i]);
+            }
+
+            for (int i = 0; i < photo.length; i++) {
+                int score = 0;
+                for (int j = 0; j < photo[i].length; j++) {
+                    score += map.getOrDefault(photo[i][j], 0);
+                }
+                answer[i] = score;
+            }
+
+            return answer;
+        }
+    }
+}
