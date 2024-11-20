a1b8aaee92c9d579668d6f9cbab4e7a269c767c9
jongsikk <xxx_@kakao.com>
2024-11-20 15:31:06 +0900

    241120 Programmers_예산

---
+++ programmers예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class programmers예산 {
+    class Solution {
+        public int solution(int[] d, int budget) {
+            int answer = 0;
+
+            Arrays.sort(d);
+            for (int i = 0; i < d.length; i++) {
+                if (budget - d[i] < 0) {
+                    break;
+                }
+                answer++;
+                budget -= d[i];
+            }
+
+            return answer;
+        }
+    }
+}
