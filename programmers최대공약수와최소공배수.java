e3c9d6a0a6d9c747946b3cdd4300f59490913eea
jongsikk <xxx_@kakao.com>
2024-11-23 00:57:06 +0900

    241123 Programmers_최대공약수와 최소공배수

---
+++ programmers최대공약수와최소공배수.java
@@ -0,0 +1,15 @@
+public class programmers최대공약수와최소공배수 {
+    class Solution {
+        public int[] solution(int n, int m) {
+            int[] answer = new int[2];
+            for (int i = 1; i <= Math.min(n, m); i++) {
+                if (n % i == 0 && m % i == 0) {
+                    answer[0] = i;
+                }
+            }
+            answer[1] = n * m / answer[0];
+
+            return answer;
+        }
+    }
+}
