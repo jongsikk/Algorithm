beadb17e77330869eb087d51642bf9e725c4fb87
jongsikk <xxx_@kakao.com>
2025-01-17 22:15:10 +0900

    250117 Programmers_두 수의 나눗셈

---
+++ programmers두수의나눗셈.java
@@ -0,0 +1,7 @@
+public class programmers두수의나눗셈 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            return (int) (((double) num1 / num2) * 1000);
+        }
+    }
+}
