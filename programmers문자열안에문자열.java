80402f6b3e4c46d6dbd6f4782c842cb0645cb2be
jongsikk <xxx_@kakao.com>
2025-01-31 19:34:42 +0900

    250131 Programmers_문자열안에 문자열

---
+++ programmers문자열안에문자열.java
@@ -0,0 +1,7 @@
+public class programmers문자열안에문자열 {
+    class Solution {
+        public int solution(String str1, String str2) {
+            return str1.contains(str2) ? 1 : 2;
+        }
+    }
+}
