6cca0ff8a5c31b1c8da0419fd0cd26b3e78d81a3
jongsikk <xxx_@kakao.com>
2025-02-22 17:47:55 +0900

    250222 Programmers_특정 문자 제거하기

---
+++ programmers특정문자제거하기.java
@@ -0,0 +1,7 @@
+public class programmers특정문자제거하기 {
+    class Solution {
+        public String solution(String my_string, String letter) {
+            return my_string.replaceAll(letter, "");
+        }
+    }
+}
