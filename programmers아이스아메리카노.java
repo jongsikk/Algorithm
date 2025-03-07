0b99dab08299cbf1454f6637a977db9e12b8d933
jongsikk <xxx_@kakao.com>
2025-03-07 23:29:04 +0900

    2500307 Programmers_아이스 아메리카노

---
+++ programmers아이스아메리카노.java
@@ -0,0 +1,7 @@
+public class programmers아이스아메리카노 {
+    class Solution {
+        public int[] solution(int money) {
+            return new int[] { money / 5500, money % 5500 };
+        }
+    }
+}
