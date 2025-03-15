a9a9f9bdb5842330e3e797dedc82757ef907fbf2
jongsikk <xxx_@kakao.com>
2025-03-15 19:52:03 +0900

    250315 Programmers_모음 제거

---
+++ programmers모음제거.java
@@ -0,0 +1,15 @@
+public class programmers모음제거 {
+    class Solution {
+        public String solution(String my_string) {
+            StringBuffer sb = new StringBuffer();
+            for (char c : my_string.toCharArray()) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    continue;
+                }
+                sb.append(c);
+            }
+
+            return sb.toString();
+        }
+    }
+}
