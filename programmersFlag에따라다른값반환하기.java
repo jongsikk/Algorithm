e515a4d5dcb6c1b86d27cdf651f3b3aed60da7ec
jongsikk <xxx_@kakao.com>
2025-03-26 23:13:09 +0900

    250326 Programmers_flag에 따라 다른 값 반환하기

---
+++ programmersFlag에따라다른값반환하기.java
@@ -0,0 +1,7 @@
+public class programmersFlag에따라다른값반환하기 {
+    class Solution {
+        public int solution(int a, int b, boolean flag) {
+            return flag ? a + b : a - b;
+        }
+    }
+}
