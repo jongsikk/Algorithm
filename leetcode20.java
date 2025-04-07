68d84097a2d7579a1ef00950bf05cd30d4422152
jongsikk <xxx_@kakao.com>
2025-04-07 13:58:30 +0900

    250407 LeetCode20_Valid Parentheses

---
+++ leetcode20.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class leetcode20 {
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                    continue;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+
+            return stack.size() == 0;
+        }
+    }
+}
