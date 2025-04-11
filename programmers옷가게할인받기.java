55156ae8eac71ea0465f4cd451a6d94d0fac38d7
jongsikk <xxx_@kakao.com>
2025-04-11 22:22:41 +0900

    250411 Programmers_옷가게 할인 받기

---
+++ programmers옷가게할인받기.java
@@ -0,0 +1,15 @@
+public class programmers옷가게할인받기 {
+    class Solution {
+        public int solution(int price) {
+            if (price >= 500000) {
+                return (int) (price * 0.8);
+            } else if (price >= 300000) {
+                return (int) (price * 0.9);
+            } else if (price >= 100000) {
+                return (int) (price * 0.95);
+            } else {
+                return price;
+            }
+        }
+    }
+}
