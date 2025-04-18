d80ed1e831a30b34541daf2d3e9720e670098120
jongsikk <xxx_@kakao.com>
2025-04-18 22:02:41 +0900

    250418 LeetCode287_Find the Duplicate Number

---
+++ leetcode287.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class leetcode287 {
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int i : nums) {
+                if (set.contains(i)) {
+                    return i;
+                }
+                set.add(i);
+            }
+            return -1;
+        }
+    }
+}
