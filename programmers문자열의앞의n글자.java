eab14a8e451c4bbf03165db456f939766a0f66cb
jongsikk <xxx_@kakao.com>
2025-05-01 13:17:30 +0900

    250501 Programmers_문자열의 앞의 n글자

---
+++ programmers문자열의앞의n글자.java
@@ -0,0 +1,7 @@
+public class programmers문자열의앞의n글자 {
+    class Solution {
+        public String solution(String my_string, int n) {
+            return my_string.substring(0, n);
+        }
+    }
+}
