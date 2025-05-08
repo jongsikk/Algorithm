4a53ada635bcbd0dc1b65d47c5fd79338f8ab2a1
jongsikk <xxx_@kakao.com>
2025-05-08 23:59:35 +0900

    250508 Programmers_홀짝 구분하기

---
+++ programmers홀짝구분하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class programmers홀짝구분하기 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if (n % 2 == 0) {
+                System.out.println(n + " is even");
+            } else {
+                System.out.println(n + " is odd");
+            }
+        }
+    }
+}
