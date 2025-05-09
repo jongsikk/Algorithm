131abe8ea92a6767deb6931f5926dd6003806210
jongsikk <xxx_@kakao.com>
2025-05-09 23:53:22 +0900

    250509 Programmers_문자열 붙여서 출력하기

---
+++ programmers문자열붙여서출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class programmers문자열붙여서출력하기 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            String b = sc.next();
+            System.out.println(a + b);
+        }
+    }
+}
